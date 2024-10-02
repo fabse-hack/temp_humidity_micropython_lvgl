63bca066bc53741abac67331796201036d5a20c6
Fabse <fabi@nomnom.de>
2024-10-02 22:36:45 +0200

    update

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import lvgl as lv
 import lcd_bus
-import time
 import ili9341
 from machine import SPI, Pin
 import machine
@@ -11,6 +10,7 @@ import dht
 import asyncio
 import utime
 import ntptime
+import neopixel
 
 spi_bus = SPI.Bus(
     host=1,
@@ -49,7 +49,7 @@ dispp = lv.display_get_default()
 theme = lv.theme_default_init(dispp, lv.palette_main(lv.PALETTE.BLUE), lv.palette_main(lv.PALETTE.RED), True, lv.font_default())
 dispp.set_theme(theme)
 
-#################################### temperature chart ###########################################
+# ################################### temperature chart #####################
 ui_TemperatureChart = lv.chart(scrn)
 ui_TemperatureChart.set_width(184)
 ui_TemperatureChart.set_height(63)
@@ -67,7 +67,7 @@ ui_TemperatureChart_Xaxis.set_mode(lv.scale.MODE.HORIZONTAL_BOTTOM)
 ui_TemperatureChart_Xaxis.set_size(lv.pct(100), 50)
 ui_TemperatureChart_Xaxis.set_align(lv.ALIGN.BOTTOM_MID)
 ui_TemperatureChart_Xaxis.set_y(50 + ui_TemperatureChart.get_style_pad_bottom(lv.PART.MAIN) + ui_TemperatureChart.get_style_border_width(lv.PART.MAIN))
-# ui_TemperatureChart_Xaxis.set_range(0, 4)
+
 ui_TemperatureChart_Xaxis.set_total_tick_count(10)
 ui_TemperatureChart_Xaxis.set_major_tick_every(2)
 ui_TemperatureChart_Xaxis.set_style_line_width(0, lv.PART.MAIN)
@@ -82,9 +82,7 @@ ui_TemperatureChart_Yaxis1.set_mode(lv.scale.MODE.VERTICAL_LEFT)
 ui_TemperatureChart_Yaxis1.set_size(50, lv.pct(100))
 ui_TemperatureChart_Yaxis1.set_align(lv.ALIGN.LEFT_MID)
 ui_TemperatureChart_Yaxis1.set_x(-50 - ui_TemperatureChart.get_style_pad_left(lv.PART.MAIN) - ui_TemperatureChart.get_style_border_width(lv.PART.MAIN) + 2)
-# ui_TemperatureChart_Yaxis1.set_range(10, 30)
-# ui_TemperatureChart_Yaxis1.set_total_tick_count((3 - 1 if 3 > 0 else 0) * 5 + 1)
-# ui_TemperatureChart_Yaxis1.set_major_tick_every(5 if 5 >= 1 else 1)
+
 ui_TemperatureChart_Yaxis1.set_style_line_width(0, lv.PART.MAIN)
 ui_TemperatureChart_Yaxis1.set_style_line_width(1, lv.PART.ITEMS)
 ui_TemperatureChart_Yaxis1.set_style_line_width(1, lv.PART.INDICATOR)
@@ -110,15 +108,12 @@ ui_HumidityChart.set_y(60)
 ui_HumidityChart.set_align(lv.ALIGN.CENTER)
 ui_HumidityChart.add_flag(lv.obj.FLAG.OVERFLOW_VISIBLE)
 ui_HumidityChart.set_type(lv.chart.TYPE.LINE)
-# ui_HumidityChart.set_range(lv.chart.AXIS.PRIMARY_Y, 10, 30)
-# ui_HumidityChart.set_range(lv.chart.AXIS.SECONDARY_Y, 10, 30)
 
 ui_HumidityChart_Xaxis = lv.scale(ui_HumidityChart)
 ui_HumidityChart_Xaxis.set_mode(lv.scale.MODE.HORIZONTAL_BOTTOM)
 ui_HumidityChart_Xaxis.set_size(lv.pct(100), 50)
 ui_HumidityChart_Xaxis.set_align(lv.ALIGN.BOTTOM_MID)
 ui_HumidityChart_Xaxis.set_y(50 + ui_HumidityChart.get_style_pad_bottom(lv.PART.MAIN) + ui_HumidityChart.get_style_border_width(lv.PART.MAIN))
-# ui_HumidityChart_Xaxis.set_range(0, 4)
 ui_HumidityChart_Xaxis.set_total_tick_count(10)
 ui_HumidityChart_Xaxis.set_major_tick_every(2)
 ui_HumidityChart_Xaxis.set_style_line_width(0, lv.PART.MAIN)
@@ -133,9 +128,7 @@ ui_HumidityChart_Yaxis1.set_size(50, lv.pct(100))
 ui_HumidityChart_Yaxis1.set_align(lv.ALIGN.LEFT_MID)
 
 ui_HumidityChart_Yaxis1.set_x(-50 - ui_HumidityChart.get_style_pad_left(lv.PART.MAIN) - ui_HumidityChart.get_style_border_width(lv.PART.MAIN) + 2)
-# ui_HumidityChart_Yaxis1.set_range(10, 30)
-# ui_HumidityChart_Yaxis1.set_total_tick_count((3 - 1 if 3 > 0 else 0) * 5 + 1)
-# ui_HumidityChart_Yaxis1.set_major_tick_every(5 if 5 >= 1 else 1)
+
 ui_HumidityChart_Yaxis1.set_style_line_width(0, lv.PART.MAIN)
 ui_HumidityChart_Yaxis1.set_style_line_width(1, lv.PART.ITEMS)
 ui_HumidityChart_Yaxis1.set_style_line_width(1, lv.PART.INDICATOR)
@@ -163,9 +156,8 @@ ui_Temperature.set_style_text_color(lv.color_hex(0x0C00FF), lv.PART.MAIN | lv.ST
 ui_Temperature.set_style_text_opa(255, lv.PART.MAIN| lv.STATE.DEFAULT)
 
 ui_Temp_data = lv.label(scrn)
-# ui_Temp_data.set_text("t_data")
 ui_Temp_data.set_width(lv.SIZE_CONTENT)
-ui_Temp_data.set_height(lv.SIZE_CONTENT) 
+ui_Temp_data.set_height(lv.SIZE_CONTENT)
 ui_Temp_data.set_x(-102)
 ui_Temp_data.set_y(-38)
 ui_Temp_data.set_align(lv.ALIGN.CENTER)
@@ -184,7 +176,6 @@ ui_Humidity.set_style_text_color(lv.color_hex(0xFF0000), lv.PART.MAIN | lv.STATE
 ui_Humidity.set_style_text_opa(255, lv.PART.MAIN| lv.STATE.DEFAULT)
 
 ui_Humidity_data = lv.label(scrn)
-# ui_Humidity_data.set_text("h_data")
 ui_Humidity_data.set_width(lv.SIZE_CONTENT)
 ui_Humidity_data.set_height(lv.SIZE_CONTENT)
 ui_Humidity_data.set_x(-104)
@@ -196,7 +187,6 @@ ui_Humidity_data.set_style_text_font(lv.font_montserrat_16, lv.PART.MAIN | lv.ST
 
 
 ui_Time = lv.label(scrn)
-# ui_Time.set_text("Time:")
 ui_Time.set_width(lv.SIZE_CONTENT)
 ui_Time.set_height(lv.SIZE_CONTENT)
 ui_Time.set_x(-76)
@@ -213,7 +203,6 @@ ui_Wifi.set_y(-91)
 ui_Wifi.set_align(lv.ALIGN.CENTER)
 
 ui_connected = lv.label(scrn)
-# ui_connected.set_text("connected")
 ui_connected.set_width(lv.SIZE_CONTENT)
 ui_connected.set_height(lv.SIZE_CONTENT)
 ui_connected.set_x(-60)
@@ -229,7 +218,6 @@ ui_MQTT.set_y(-107)
 ui_MQTT.set_align(lv.ALIGN.CENTER)
 
 ui_enabled = lv.label(scrn)
-# ui_enabled.set_text("enabled")
 ui_enabled.set_width(lv.SIZE_CONTENT)
 ui_enabled.set_height(lv.SIZE_CONTENT)
 ui_enabled.set_x(104)
@@ -245,7 +233,6 @@ ui_IP_.set_y(-91)
 ui_IP_.set_align(lv.ALIGN.CENTER)
 
 ui_ip_data = lv.label(scrn)
-# ui_ip_data.set_text("127.0.0.1")
 ui_ip_data.set_width(lv.SIZE_CONTENT)
 ui_ip_data.set_height(lv.SIZE_CONTENT)
 ui_ip_data.set_x(88)
@@ -258,7 +245,6 @@ ui_punkt1.set_height(10)
 ui_punkt1.set_x(-1)
 ui_punkt1.set_y(-107)
 ui_punkt1.set_align(lv.ALIGN.CENTER)
-# ui_punkt1.set_style_bg_color(lv.color_make(0, 0, 255), lv.PART.MAIN | lv.STATE.DEFAULT)
 
 ui_punkt2 = lv.obj(scrn)
 ui_punkt2.set_width(10)
@@ -266,7 +252,6 @@ ui_punkt2.set_height(10)
 ui_punkt2.set_x(-1)
 ui_punkt2.set_y(-91)
 ui_punkt2.set_align(lv.ALIGN.CENTER)
-# ui_punkt2.set_style_bg_color(lv.color_make(0, 255, 0), lv.PART.MAIN | lv.STATE.DEFAULT)
 
 ui_punkt3 = lv.obj(scrn)
 ui_punkt3.set_width(10)
@@ -274,7 +259,6 @@ ui_punkt3.set_height(10)
 ui_punkt3.set_x(-150)
 ui_punkt3.set_y(-107)
 ui_punkt3.set_align(lv.ALIGN.CENTER)
-# ui_punkt3.set_style_bg_color(lv.color_make(255, 0, 0), lv.PART.MAIN | lv.STATE.DEFAULT)
 
 ui_punkt4 = lv.obj(scrn)
 ui_punkt4.set_width(10)
@@ -282,8 +266,6 @@ ui_punkt4.set_height(10)
 ui_punkt4.set_x(-150)
 ui_punkt4.set_y(-91)
 ui_punkt4.set_align(lv.ALIGN.CENTER)
-# ui_punkt4.set_style_bg_color(lv.color_make(255, 0, 0), lv.PART.MAIN | lv.STATE.DEFAULT)
-
 
 ui_frame_status = lv.obj(scrn)
 ui_frame_status.set_width(318)
@@ -385,7 +367,7 @@ class Uhrzeit:
 
     def ist_sommerzeit(self, zeit):
         """Bestimmt, ob Sommerzeit (MESZ) angewendet wird."""
-        jahr, monat, tag = zeit[0], zeit[1], zeit[2]
+        monat, tag = zeit[1], zeit[2]
 
         if monat > 3 and monat < 10:
             return True
@@ -528,6 +510,12 @@ async def data_to_lvgl_every_hours():
         ui_Temp_data.set_text(f"{temperature} °C")
         humidity = int(d.humidity())
         ui_Humidity_data.set_text(f"{humidity} %")
+        
+        led.fill((0, 0, 255)).write()
+
+        await asyncio.sleep(3)
+
+        led.fill((0, 255, 0)).write()
 
         await asyncio.sleep(3)
 
@@ -555,7 +543,6 @@ async def data_to_lvgl_every_second():
         await asyncio.sleep(1)
 
 
-
 async def main():
     task1 = asyncio.create_task(data_to_lvgl_every_hours())
     task2 = asyncio.create_task(data_to_lvgl_every_second())
@@ -567,6 +554,8 @@ async def main():
 
 if __name__ == "__main__":
 
+    led = neopixel.NeoPixel(Pin(2), 1)
+    led.fill((255, 255, 255)).write()
 
     th = task_handler.TaskHandler()
 
@@ -578,22 +567,22 @@ if __name__ == "__main__":
     print("Initial Temperature: {:.2f} °C".format(temp_init))
     print("Initial Humidity: {:.2f} %".format(humidity_init))
 
-    wifi_manager = WiFiManager('idontknow', 'dumdidum', 'temp_humidity', '12345678')
+    wifi_manager = WiFiManager('idontknow', 'xxxxxx', 'temp_humidity', '12345678')
     ssid, ip_address, status = wifi_manager.configure_wifi()
     print(f'SSID: {ssid}, IP-Adresse: {ip_address}, Status: {status}')
+    led.fill((255, 0, 0)).write()
 
     mqtt_publisher = MQTTSensorPublisher(
-                                        broker_address="192.168.178.103",
+                                        broker_address="xxxxx",
                                         port=1883,
-                                        topic="sensor/temperature",
-                                        username="homeassistant",
-                                        password="dumdidum",
+                                        topic="humi_temp",
+                                        username="xxxxxx",
+                                        password="xxxxxx",
                                         sensor_callback=sensor_callback
                                         )
 
     mqtt_publisher.connect()
-    
-    
+
     uhr = Uhrzeit()
     print(uhr)
     temperature_list = [25] * 10
